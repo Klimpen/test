e224fa3e690c7c2782bf03be4ddf3b43fe33b37d
Dominik Schmid <DominikSchmid93@gmail.com>
2017-09-14 00:50:08 +1000

    added animated colors
    fixed up var -> const

--- script.js
+++ script.js
@@ -1,50 +1,65 @@
 var data = [];
 
 for(var i=0; i<90; i++){
-	data[i] = i+1;
+    data[i] = i+1;
 }
 
 data = d3.shuffle(data);
 
-var barWidth = 20,
-    height = 420;
+const padding = 10; // TODO Do this with css eventually
+const barWidth = (window.innerWidth - padding) / data.length;
+const height = 420;
 
 // values used for the specific sort algorithm
-var minValue = 0;
-var currentPosition = 1;
+const minValue = 0;
+const currentPosition = 1;
 
-var y = d3.scale.linear()
+const y = d3.scale.linear()
     .domain([0, d3.max(data)])
     .range([0, height]);
 
-var chart = d3.select(".chart")
+const chart = d3.select(".chart")
     .attr("width", barWidth * data.length)
     .attr("height", height);
 
-var bar = chart.selectAll("g")
+const bar = chart.selectAll("g")
     .data(data)
-  .enter().append("g")
-    .attr("transform", function(d, i) { return "translate(" + i * barWidth + "," + (height-y(d)) + ")"; });
+    .enter().append("g")
+        .attr("transform", function(d, i) {
+            return "translate(" + i * barWidth + "," + (height-y(d)) + ")";
+        })
 
-bar.append("rect")
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+}
+
+const rect = bar.append("rect")
     .attr("width", barWidth - 1)
-    .attr("height", y);
+    .attr("height", y)
 
 function update(){
-	// each update should be equal to a single compare
-	// should color the two bars one way if a>b
-	// should swap and colour differently again if b>a
-	var t = d3.transition()
-		.duration(750);
-	compare();
-	data.attr("class", "update")
-		.transition(t);
+    const compare_top = getRandomInt(0, data.length); // TODO initialize from compare() somehow
+    const compare_bot = getRandomInt(0, data.length);
+    rect.style('fill', (d, i) => {
+        if (i == compare_top) return 'salmon';
+        if (i == compare_bot) return 'seagreen';
+        return 'cornflowerblue';
+    })
+    // each update should be equal to a single compare
+    // should color the two bars one way if a>b
+    // should swap and colour differently again if b>a
+
+    // compare();
+
 }
 
 function compare(){
-	// implement whatever sort algorithm here
+    // implement whatever sort algorithm here
 }
 
 update();
 
-d3.interval(update(), 1500000);
+setInterval(update, 0.3*1000);
